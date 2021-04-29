e5cd24cd5529c6def0f8e73a8810bc59dc296e94
Yifei <77204643+Yifei29@users.noreply.github.com>
2021-04-29 10:48:19 -0400

    Create OnPlayerJoinScript.lua

---
+++ Resources/ScriptLibrary/OnPlayerJoinScript.lua
@@ -0,0 +1,21 @@
+scriptInfo(DO NOT PUT IN YOUR SCRIPT){
+  scriptType = Regular/ServerScript
+  typeTime = ~30s
+  difficulty = Easy
+  codeLanguage = RobloxStudio Lua
+}
+
+//[Before you start, do these steps:
+1. Make a script in ServerScriptService
+2. Open the script
+3. Lets start!
+]
+    
+    
+function PlayerJoin()
+      //Put your code here, for example, we are going to put print()
+      print(:D)
+      
+      end)
+  
+game.Players.PlayerAdded:Connect(PlayerJoin())  
